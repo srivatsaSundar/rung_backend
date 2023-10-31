4b6400d1338aa19518c623b701bca976a9e38235
Srivatsa S <136162339+srivatsa-ibots@users.noreply.github.com>
2023-10-31 07:01:14 +0530

    user

--- rung/views.py
+++ rung/views.py
@@ -66,10 +66,19 @@ def create_contact_us(request):
 @api_view(['POST'])
 def user(request):
     if request.method == 'POST':
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        data = request.data 
+        try:
+            order = Order(
+                person_name=data.get('person_name'),
+                email=data.get('email'),
+                address=data.get('address'),
+                postal_code=data.get('postal_code'),
+                city=data.get('city'),
+                phone_number=data.get('phone_number'),
+                company_name=data.get('company_name'),
+                delivery_option=data.get('delivery_option')
+            )
+            order.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            print(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
